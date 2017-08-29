1706a307fb74f03ed1a9c10c5771f2ff3ee5f206
UnsignedInt8 <unsignedint8@gmail.com>
2017-08-29 14:36:06 +0800

    layout/fonts/assets

--- app/src/main/java/dwallet/app/android/MainActivity.kt
+++ app/src/main/java/dwallet/app/android/MainActivity.kt
@@ -1,13 +1,25 @@
 package dwallet.app.android
 
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Window
+import android.widget.TextView
+import dwallet.core.bitcoin.application.wallet.Wallet
+import dwallet.core.crypto.Crypto
 
 class MainActivity : AppCompatActivity() {
+
+    init {
+        Crypto.setupCryptoProvider()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.textView_AppTitle).setTypeface(Typeface.createFromAsset(assets, "fonts/PoiretOne-Regular.ttf"))
     }
 
 }
