c990fe81a0077c20ee8f80663cf5ae95e5351c0c
UnsignedInt8 <unsignedint8@gmail.com>
2017-08-29 15:24:47 +0800

    add strings

--- app/src/main/java/dwallet/app/android/MainActivity.kt
+++ app/src/main/java/dwallet/app/android/MainActivity.kt
@@ -3,10 +3,7 @@ package dwallet.app.android
 import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Window
 import android.widget.TextView
-import dwallet.core.bitcoin.application.wallet.Wallet
 import dwallet.core.crypto.Crypto
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +15,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<TextView>(R.id.textView_AppTitle).setTypeface(Typeface.createFromAsset(assets, "fonts/PoiretOne-Regular.ttf"))
+        initUI()
     }
 
+    private fun initUI(){
+        findViewById<TextView>(R.id.textView_AppTitle).setTypeface(Typeface.createFromAsset(assets, "fonts/PoiretOne-Regular.ttf"))
+    }
 }
